439aa87136cbeda913406bea962c124e5e65d362
Osahenrumwen Aigbogun <leonardosahon@gmail.com>
2024-01-20 12:28:38 +0100

    added support for AWS SDK

---
+++ src/Libs/Aws/Enums/AwsS3Client.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BrickLayer\Lay\Libs\Aws\Enums;
+
+enum AwsS3Client
+{
+    case R2;
+    case S3;
+}
---
+++ src/Libs/Aws/LayAws.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace BrickLayer\Lay\Libs\Aws;
+
+use Aws\Credentials\Credentials;
+use BrickLayer\Lay\Core\Exception;
+use BrickLayer\Lay\Core\Traits\IsSingleton;
+use BrickLayer\Lay\Libs\Aws\Enums\AwsS3Client;
+
+class LayAws
+{
+    use IsSingleton;
+    
+    public static AwsS3Client $type;
+    public static array $credentials;
+
+    public static function exception(string $title, string $message) : void
+    {
+        Exception::throw_exception($message, "AWS_" . $title);
+    }
+
+    public static function init( AwsS3Client $type = AwsS3Client::R2 ) : self
+    {
+        $credentials = new Credentials($_ENV['AWS_ACCESS_KEY_ID'],$_ENV['AWS_ACCESS_KEY_SECRET']);
+        $region = $_ENV['AWS_REGION'] ?? 'auto';
+
+        if($type == AwsS3Client::S3)
+            $region = $_ENV['AWS_REGION'] ?? 'us-east-1';
+
+        $options = [
+            'region' => $region,
+            'version' => 'latest',
+            'credentials' => $credentials
+        ];
+
+        if($type == AwsS3Client::R2) {
+            if (!isset($_ENV['CLOUDFLARE_ACCOUNT_ID']))
+                self::exception("RequiredKeyNotSet", "`CLOUDFLARE_ACCOUNT_ID` env variable is not set. Please update your .env file and include it");
+
+            $options['endpoint'] = "https://{$_ENV['CLOUDFLARE_ACCOUNT_ID']}.r2.cloudflarestorage.com";
+        }
+
+        self::$credentials = $options;
+
+        return self::new();
+    }
+}
