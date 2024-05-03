ea521ca23885e84f9ac4c979ffe437331e2886cd
Osahenrumwen Aigbogun <leonardosahon@gmail.com>
2024-05-03 16:02:57 +0100

    Added automatic domain base prepend on src accessed through ViewSrc class

--- src/Core/View/ViewSrc.php
+++ src/Core/View/ViewSrc.php
@@ -5,7 +5,7 @@ namespace BrickLayer\Lay\Core\View;
 use BrickLayer\Lay\Core\LayConfig;
 
 final class ViewSrc {
-    public static function gen(string $src) : string
+    public static function gen(string $src, bool $prepend_domain_base = true) : string
     {
         $src = SrcFilter::go($src);
 
@@ -13,8 +13,12 @@ final class ViewSrc {
 
         $base = $client->domain->domain_base;
 
-        if(!str_starts_with($src, $base))
-            return $src;
+        if(!str_starts_with($src, $base)) {
+            if(!$prepend_domain_base || str_starts_with($src, "http") || str_starts_with($src, "data:"))
+                return $src;
+
+            return $client->domain->domain_base . $src;
+        }
 
         $local_file = str_replace($base, $client->domain->domain_root, $src);
 
