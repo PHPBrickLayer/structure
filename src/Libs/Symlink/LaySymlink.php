ff8f99446639f6975e63785720a0c1fd97d6e576
Osahenrumwen Aigbogun <leonardosahon@gmail.com>
2024-06-27 09:49:30 +0100

    Added better reporting on laysymlink

--- src/Libs/Symlink/LaySymlink.php
+++ src/Libs/Symlink/LaySymlink.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 namespace BrickLayer\Lay\Libs\Symlink;
 
+use BrickLayer\Lay\Core\Exception;
 use BrickLayer\Lay\Core\LayConfig;
 use BrickLayer\Lay\Core\Traits\IsSingleton;
 
@@ -10,11 +11,22 @@ class LaySymlink {
 
     public static function make(string $src, string $dest, SymlinkTypes $type) : void
     {
+        $exe = fn() => Exception::new()->use_exception(
+            "SymlinkFailed",
+            "Could not successfully create a symbolic link. \n"
+            . "SRC: $src \n"
+            . "DEST: $dest \n"
+            . "Kindly confirm if the src exists and the destination's directory exists"
+            ,
+        );
+
         if(LayConfig::new()->get_os() == "WINDOWS") {
+            // TODO: Study the behaviour of symlink on windows and catch the necessary errors
             exec("mklink " . $type->value . " $dest $src");
             return;
         }
-        
-        symlink($src, $dest);
+
+        if(!@symlink($src, $dest))
+            $exe();
     }
 }
