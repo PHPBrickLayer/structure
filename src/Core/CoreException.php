39e902818e7ffbacddcfefb936f071e8be6a68a6
Osahenrumwen Aigbogun <leonardosahon@gmail.com>
2024-07-08 19:36:19 +0100

    Removed SQL dependency from exception

--- src/Core/CoreException.php
+++ src/Core/CoreException.php
@@ -389,9 +389,6 @@ class CoreException
         }
 
         if ($act == "kill") {
-            if(SQL::is_connected())
-                SQL::new()->close();
-
             self::$already_caught = true;
             error_reporting(0);
             ini_set('error_log', false);
