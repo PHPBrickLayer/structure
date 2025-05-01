bf42adad8a52324912ae9e021dfad9f2b5716cd7
Osahenrumwen Aigbogun <leonardosahon@gmail.com>
2025-05-01 08:17:32 +0100

    Updated VCM, improved LayCrypt and ORM

--- src/Core/Traits/ValidateCleanMap.php
+++ src/Core/Traits/ValidateCleanMap.php
@@ -20,7 +20,7 @@ use Exception;
 /**
  * @psalm-type VcmRules array{
  *     required?: bool,
- *     db_col_required?: bool,
+ *     alias_required?: bool,
  *     clean?: bool|array{
  *       escape: EscapeType,
  *       strict: bool,
@@ -44,7 +44,7 @@ trait ValidateCleanMap {
     private static ?bool $_break_validation = false;
 
     private static ?bool $_required;
-    private static ?bool $_db_col_required;
+    private static ?bool $_alias_required;
     private static array|bool|null $_clean_by_default;
     private static ?string $_sub_dir;
     private static ?array $_allowed_types;
@@ -257,6 +257,11 @@ trait ValidateCleanMap {
         if(isset($options['is_email']) && !filter_var($value, FILTER_VALIDATE_EMAIL))
             $add_to_entry = $this->__add_error($field, "Received an invalid email format for: $field_name");
 
+        if(isset($options['is_bool'])) {
+            if(!in_array(strtolower($value . ''), ['true', 'false', '1', '0']))
+                $add_to_entry = $this->__add_error($field, "$field_name is not a valid boolean");
+        }
+
         if(isset($options['is_num']) && !is_numeric($value))
             $add_to_entry = $this->__add_error($field, "$field_name is not a valid number");
 
@@ -317,6 +322,7 @@ trait ValidateCleanMap {
      *    field: string,
      *    field_name?: string,
      *    required_message?: string,
+     *    alias?: string,
      *    db_col?: string,
      *    before_validate?: callable(mixed) : string,
      *    before_clean?: callable(mixed) : string,
@@ -332,6 +338,7 @@ trait ValidateCleanMap {
      *    is_email?: bool,
      *    is_name?: bool,
      *    is_num?: bool,
+     *    is_bool?: bool,
      *    is_date?: bool,
      *    is_datetime?: bool,
      *    is_file?: bool,
@@ -396,12 +403,6 @@ trait ValidateCleanMap {
 
             $x['add_to_entry'] = true;
             $x['apply_clean'] = false;
-
-            if(!isset($options['db_col']))
-                LayException::throw_exception(
-                    "DB column for field [$field] must be specified. This field has the [] symbol, and this symbol is invalid for a db column name",
-                    "VCM::Error"
-                );
         } else {
             $x = $this->__validate(
                 $field, $value,
@@ -428,18 +429,18 @@ trait ValidateCleanMap {
         }
 
         //TODO: Depreciate fun option
-        if(isset($options['fun']) || isset($options['before_clean']))
-            $value = ($options['fun'] ?? $options['before_clean'])($value);
+        if(isset($options['before_clean']) || isset($options['fun']))
+            $value = ($options['before_clean'] ?? $options['fun'])($value);
 
         if($apply_clean) {
             // Clean and Map field
-            $clean = $options['clean'] ?? self::$_clean_by_default ?? null;
+            $clean = $options['clean'] ?? self::$_clean_by_default ?? true;
 
             if ($clean) {
                 $clean_type = is_array($clean) ? ($clean['escape'] ?? EscapeType::STRIP_TRIM_ESCAPE) : EscapeType::STRIP_TRIM_ESCAPE;
                 $strict = $is_required ? ($clean['strict'] ?? false) : false;
 
-                if (is_numeric($value))
+                if (is_numeric($value) || is_bool($value))
                     $strict = false;
 
                 $value = Escape::clean($value, $clean_type, [
@@ -451,21 +452,23 @@ trait ValidateCleanMap {
         if(isset($options['after_clean']))
             $value = $options['after_clean']($value);
 
-        if(self::$_db_col_required && !isset($options['db_col']))
+        $alias = $options['alias'] ?? $options['db_col'] ?? null;
+
+        if(self::$_alias_required && !$alias)
             LayException::throw_exception(
-                "DB column for field [$field] was not specified and is required by the validation rule",
+                "An alias for field [$field] was not specified and is required by the validation rule. Please set one using the `alias` key",
                 "VCM::Error"
             );
 
         $return_struct = $options['return_struct'] ?? self::$_return_struct ?? null;
 
         if($return_struct)
-            $value = $return_struct($value, $options['db_col'] ?? $field, $options);
+            $value = $return_struct($value, $alias ?? $field, $options);
 
         $result_is_assoc = $options['result_is_assoc'] ?? self::$_result_is_assoc;
 
         if($result_is_assoc)
-            self::$_entries[$options['db_col'] ?? $field] = $value;
+            self::$_entries[$alias ?? $field] = $value;
         else
             self::$_entries[] = $value;
 
@@ -478,7 +481,7 @@ trait ValidateCleanMap {
      * @param array{
      *      required?: bool,
      *      result_is_assoc?: bool,
-     *      db_col_required?: bool,
+     *      alias_required?: bool,
      *      clean?: bool|array{
      *        escape: EscapeType|array<int,EscapeType>,
      *        strict: bool,
@@ -501,7 +504,7 @@ trait ValidateCleanMap {
     {
         self::$_required = $options['required'] ?? null;
         self::$_clean_by_default = $options['clean'] ?? null;
-        self::$_db_col_required = $options['db_col_required'] ?? null;
+        self::$_alias_required = $options['alias_required'] ?? null;
         self::$_sub_dir = $options['sub_dir'] ?? null;
         self::$_allowed_types = $options['allowed_types'] ?? $options['allowed_extensions'] ?? null;
 
@@ -527,7 +530,7 @@ trait ValidateCleanMap {
      * @param null|array{
      *      required?: bool,
      *      result_is_assoc?: bool,
-     *      db_col_required?: bool,
+     *      alias_required?: bool,
      *      clean?: bool|array{
      *          escape: EscapeType|array<int,EscapeType>,
      *        strict: bool,
@@ -555,7 +558,7 @@ trait ValidateCleanMap {
         self::$_break_validation = false;
 
         self::$_required = null;
-        self::$_db_col_required = null;
+        self::$_alias_required = null;
         self::$_clean_by_default = null;
         self::$_sub_dir = null;
         self::$_allowed_types = null;
