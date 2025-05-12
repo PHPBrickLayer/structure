5dc079661c791b497e31df80939adc302979fdab
Osahenrumwen Aigbogun <leonardosahon@gmail.com>
2025-05-12 19:04:15 +0100

    ViewSrc improvement

--- src/Core/View/ViewSrc.php
+++ src/Core/View/ViewSrc.php
@@ -2,22 +2,26 @@
 declare(strict_types=1);
 namespace BrickLayer\Lay\Core\View;
 
-use BrickLayer\Lay\Core\LayConfig;
-
 final class ViewSrc {
-    public static function gen(string $src, bool $prepend_domain_base = true) : string
+    /**
+     * @param string $src
+     * @param bool $prepend_domain_base
+     * @param string|null $api_replacement A domain you want to use to replace Api in the url
+     * @return string
+     */
+    public static function gen(string $src, bool $prepend_domain_base = true, ?string $api_replacement = 'Default') : string
     {
         $src = SrcFilter::go($src);
 
         $client = DomainResource::get();
 
-        $base = $client->domain->domain_base;
+        $base = str_replace("Api", $api_replacement, $client->domain->domain_base);
 
         if(!str_starts_with($src, $base)) {
             if(!$prepend_domain_base || str_starts_with($src, "http") || str_starts_with($src, "data:"))
                 return $src;
 
-            return $client->domain->domain_base . $src;
+            return $base . $src;
         }
 
         $local_file = str_replace($base, $client->domain->domain_root, $src);
