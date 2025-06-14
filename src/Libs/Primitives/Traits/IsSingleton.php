712a3b460fb168c103ae2a9689778554c4842b5e
Osahenrumwen Aigbogun <leonardosahon@gmail.com>
2025-06-14 14:31:44 +0100

    By design Singleton should not be inherited, it's a single instance

--- src/Libs/Primitives/Traits/IsSingleton.php
+++ src/Libs/Primitives/Traits/IsSingleton.php
@@ -10,21 +10,21 @@ trait IsSingleton {
     private function __construct(){}
     private function __clone(){}
 
-    private static function SINGLETON() : static
+    private  static function SINGLETON() : static
     {
         if(!isset(static::$instance))
             static::$instance = new static();
 
-        return static::$instance;
+        return self::$instance;
     }
 
     public static function instance() : static
     {
-        return static::SINGLETON();
+        return self::SINGLETON();
     }
 
     public static function new() : static
     {
-        return static::SINGLETON();
+        return self::SINGLETON();
     }
 }
