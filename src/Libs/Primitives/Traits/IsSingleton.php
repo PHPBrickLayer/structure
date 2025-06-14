0cc1a6a2a171e0628788a31c17bb146f6d2f2105
Osahenrumwen Aigbogun <leonardosahon@gmail.com>
2025-06-14 13:47:27 +0100

    Made singleton reflect the class it is called in

--- src/Libs/Primitives/Traits/IsSingleton.php
+++ src/Libs/Primitives/Traits/IsSingleton.php
@@ -10,21 +10,21 @@ trait IsSingleton {
     private function __construct(){}
     private function __clone(){}
 
-    private  static function SINGLETON() : static
+    private static function SINGLETON() : static
     {
         if(!isset(static::$instance))
             static::$instance = new static();
 
-        return self::$instance;
+        return static::$instance;
     }
 
     public static function instance() : static
     {
-        return self::SINGLETON();
+        return static::SINGLETON();
     }
 
     public static function new() : static
     {
-        return self::SINGLETON();
+        return static::SINGLETON();
     }
 }
