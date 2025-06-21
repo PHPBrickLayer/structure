44f1cb58d89100a0f62c6bc296de124a63602acb
Osahenrumwen Aigbogun <leonardosahon@gmail.com>
2025-06-21 18:41:10 +0100

    Made exec_pre_run protected

--- src/Libs/Primitives/Abstracts/BaseModelHelper.php
+++ src/Libs/Primitives/Abstracts/BaseModelHelper.php
@@ -144,7 +144,7 @@ abstract class BaseModelHelper
         );
     }
 
-    private function exec_pre_run(SQL $db) : void
+    protected final function exec_pre_run(SQL $db) : void
     {
         foreach($this->pre_run as $query) {
             if(!($query instanceof Closure))
